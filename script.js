85240b206632510bb7c10397f7110148c50f3aea
Luis Hernandez <luishernandez@Luiss-MacBook-Pro.local>
2023-06-28 22:47:08 -0500

    my food truck website

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".icon").animate({opacity: '0'})
+  $(".menu").delay("300")
+  $(".maths-favorites").fadeIn("slow")
+  $(".food-images").animate({opacity: '0.6'})
+  $(".address").fadeIn("slow")
+  $(".hours").fadeIn("slow")
+});
